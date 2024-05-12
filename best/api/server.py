49f935251c178baf36eb83c72d6ae2e5d6e0c774
Diego Gettatelli <dgettatelli@gmail.com>
2024-05-12 19:32:20 +0200

    wip: init first fastapi endpoint

--- best/api/server.py
+++ best/api/server.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from fastapi import Body
 from trading_ig import rest
+from trading_ig_config import config
 
 app = FastAPI()
+service = rest.IGService(
+    api_key= config.api_key,
+    username= config.username,
+    password= config.password
+)
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 @app.get("/fetch-open-position")
 async def fetch_open_position():
     print(Body())
+    return Body()
+
+@app.get("/fetch/accounts")
+async def fetch_accounts():
+    result = service.fetch_accounts()
+    return result
+
